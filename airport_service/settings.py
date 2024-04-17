2e0f6cf8e6d4aab2ac0833be2351ea86692e25ef
Ant <u123@ua.fm>
2024-04-17 22:58:39 +0300

    update settings.py with AUTH_USER_MODEL

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "user",
     "airport",
 ]
 
@@ -99,6 +101,8 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+AUTH_USER_MODEL = "user.User"
 
 
 # Internationalization
