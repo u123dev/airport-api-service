1012bab31be4df4a7888baf1482a8983cf5dd2e4
Ant <u123@ua.fm>
2024-04-20 11:33:30 +0300

    update main urls

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -22,4 +22,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("user.urls", namespace="user")),
+    path("api/airport/", include("airport.urls", namespace="airport")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
