6759ea5f85a5a4bf9632fb8beb9d912517d98e53
Ant <u123@ua.fm>
2024-04-25 19:19:03 +0300

    add SECRET KEY as env variable

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -21,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!w06ih8u79bgb5&!ojsun^(kndkxiot^u6muh6^w#8!&$85+(c"
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    "django-insecure-!w06ih8u79bgb5&!ojsun^(kndkxiot^u6muh6^w#8!&$85+(c"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
